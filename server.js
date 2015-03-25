8bef27afe7f3d92268f4545c799cd9714163ef32
shiplet <shipletm11@gmail.com>
2015-03-24 22:20:14 -0600

    update

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var express = require('express'),
 
 app.use(express.static(__dirname));
 
+app.get('/js/e.json', function(req, res){
+    console.log('File requested at /js/e.js');
+    res.sendFile(__dirname + '/js/e.json');
+});
+
+
 app.use('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -12,7 +18,3 @@ app.listen(process.env.PORT || 3000, function(){
     console.log('Listening on port %d',  this.address().port, app.settings.en);
 });
 
-app.get('/js/e.js', function(req, res){
-    console.log('File requested at /js/e.js');
-    res.sendFile(__dirname + '/js/e.js');
-});
