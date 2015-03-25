2923cf135e898f9e00437d2160bf885ed02f5b73
shiplet <shipletm11@gmail.com>
2015-03-24 21:33:46 -0600

    update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080, function(){
-    console.log('Listening on port 8080');
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Listening on port %d',  this.address().port, app.settings.en);
 });
